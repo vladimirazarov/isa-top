322c9fd81a2f011bec4829433767cb4d974293ee
Vladimir Azarov <xazaro00@vutbr.cz>
2024-10-09 16:30:02 +0200

    Connection class implementation

---
+++ src/connection.cpp
@@ -0,0 +1,22 @@
+#include "connection.hpp"
+#include "connectionID.hpp"
+
+
+
+Connection::Connection(sockaddr_in6 srcEndPoint, sockaddr_in6 destEndpoint, IPFamily ipv4oripv6, Protocol protocol)
+{
+    m_ID.m_srcEndPoint = srcEndPoint;
+    m_ID.m_destEndPoint = destEndpoint;
+    m_ID.m_protocol = protocol;
+    m_ipFamily = ipv4oripv6;
+    m_bytesSent = m_bytesReceived = m_packetsSent = m_packetsReceived = 0;
+    m_first_seen = std::chrono::system_clock::now();
+    m_last_seen = std::chrono::system_clock::now();
+};
+Connection::Connection()
+{
+    m_ipFamily = IPv4;
+    m_bytesSent = m_bytesReceived = m_packetsSent = m_packetsReceived = 0;
+    m_first_seen = std::chrono::system_clock::now();
+    m_last_seen = std::chrono::system_clock::now();
+};
---
+++ src/connection.hpp
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <functional>
+#include <string>
+#include <chrono>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <cstring>
+#include <stdint.h>
+#include <unordered_map>
+#include <vector>
+#include <stdint.h>
+#include <sstream>
+#include <arpa/inet.h>  
+#include <mutex>
+#include "connectionID.hpp"
+
+
+enum IPFamily
+{
+    IPv4,
+    IPv6
+};
+// ConnectionID is the unique connection identifier of a connection: srcIP + destIP + destPort + srcPort + Protocol for TCP/UDP
+//                                                                   srcIP + destIP + protocol                      for ICMP
+
+
+class Connection
+{
+public:
+    ConnectionID m_ID;
+    IPFamily m_ipFamily;
+
+    unsigned long int m_bytesSent;
+    unsigned long int m_bytesReceived;
+    unsigned long int m_packetsSent;
+    unsigned long int m_packetsReceived;
+
+    std::chrono::system_clock::time_point m_first_seen;
+    std::chrono::system_clock::time_point m_last_seen;
+
+    Connection(sockaddr_in6 srcEndPoint, sockaddr_in6 destEndPoint, IPFamily ipFamily, Protocol protocol);
+    Connection();
+};
